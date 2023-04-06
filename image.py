84fc12d5a09b2f0acd0675479814ceed7d72595c
kostotpe <a19910304@hotmail.com>
2023-04-06 20:24:05 +0800

    v1

---
+++ image.py
@@ -0,0 +1,10 @@
+# 練習把圖案轉成黑白或灰階
+from PIL import Image
+import os
+
+
+for file in os.listdir('.'): # 在這個資料夾內的檔案
+    if file.endswith('.jpg'): # 檢查()的檔案類別
+        img2 = Image.open(file) #file path
+        img2 = img2.convert('L') # convert to L (Grayscale)
+        img2.save(file[:-4] + '_2.png') #file path # [:-4] 原檔名只取到倒數第五個
