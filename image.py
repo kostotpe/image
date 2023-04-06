8ae368295b8e98ffe04e81083ac816c3b8c71bef
kostotpe <a19910304@hotmail.com>
2023-04-06 20:39:45 +0800

    v3

--- image.py
+++ image.py
@@ -5,6 +5,6 @@ import os
 
 for file in os.listdir('orig'): # 在這個資料夾內的檔案
     if file.endswith('.jpg'): # 檢查()的檔案類別
-        img2 = Image.open('orig/' + file) #file path
+        img2 = Image.open(os.path.join('orig', file)) #file path  os.path.join合併路徑
         img2 = img2.convert('L') # convert to L (Grayscale)
-        img2.save('result/' + file[:-4] + '_2.png') #file path # [:-4] 原檔名只取到倒數第五個
+        img2.save(os.path.join('result', file[:-4] + '_2.png')) # file path # [:-4] 原檔名只取到倒數第五個
